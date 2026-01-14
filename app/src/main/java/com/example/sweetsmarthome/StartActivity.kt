632c0682fff439b0fcc5fcad1aac128049792a78
Romero <romio04pro@gmail.com>
2026-01-14 18:16:56 +0200

    Add functional

---
+++ app/src/main/java/com/example/sweetsmarthome/StartActivity.kt
@@ -0,0 +1,14 @@
+package com.example.sweetsmarthome
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.bt_def.BaseActivity
+
+class StartActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        startActivity(Intent(this, BaseActivity::class.java))
+    }
+}
---
+++ bt_def/src/main/java/com/example/bt_def/bluetooth/ConnectThread.kt
@@ -0,0 +1,41 @@
+package com.example.bt_def.bluetooth
+
+import android.bluetooth.BluetoothDevice
+import android.bluetooth.BluetoothSocket
+import android.util.Log
+import java.io.IOException
+import java.util.*
+
+class ConnectThread(device: BluetoothDevice) : Thread() {
+    private val uuid = "00001101-0000-1000-8000-00805F9B34FB"
+    private var mSocket: BluetoothSocket? = null
+    init {
+        try {
+            mSocket = device.createRfcommSocketToServiceRecord(UUID.fromString(uuid))
+        } catch (e: IOException){
+
+        } catch (se: SecurityException){
+
+        }
+    }
+
+    override fun run() {
+        try {
+            Log.d("MyLog", "Connecting...")
+            mSocket?.connect()
+            Log.d("MyLog", "Connected...")
+        } catch (e: IOException){
+            Log.d("MyLog", "Not connected...")
+        } catch (se: SecurityException){
+
+        }
+    }
+
+    fun closeConnection(){
+        try {
+            mSocket?.close()
+        } catch (e: IOException){
+
+        }
+    }
+}
