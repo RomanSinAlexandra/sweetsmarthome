48dd999596b17aa8169efc4b57293b85f10f9c8d
Romero <romio04pro@gmail.com>
2026-01-17 02:44:15 +0200

    Add functional

--- app/src/main/java/com/example/sweetsmarthome/StartActivity.kt
+++ app/src/main/java/com/example/sweetsmarthome/StartActivity.kt
@@ -1,14 +1,28 @@
 package com.example.sweetsmarthome
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.bt_def.BaseActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.sweetsmarthome.databinding.ContentStartBinding
 
 class StartActivity : AppCompatActivity() {
+
+    private lateinit var binding: ContentStartBinding
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        startActivity(Intent(this, BaseActivity::class.java))
+
+        binding = ContentStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        navController = navHostFragment.navController
+
+        binding.bottomNavigation.setupWithNavController(navController)
     }
 }
